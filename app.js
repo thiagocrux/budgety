463b699ba8adba72e10ca15280c75114653ab190
Thiago Cruz <tlcs1993@gmail.com>
2020-01-04 03:08:32 -0300

    After class 86 "Updating the Budget: Budget Controller"

--- app.js
+++ app.js
@@ -14,16 +14,30 @@ var budgetController = (function() {
         this.value = value;
     };
 
+    var calculateTotal = function(type) {
+        var sum = 0;
+
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        });
+
+        data.totals[type] = sum;
+    };
+
     var data = {
         allItems: {
             exp: [],
             inc: []
         },
 
-        total: {
+        totals: {
             exp: 0,
             inc: 0
-        }
+        },
+
+        budget: 0,
+
+        percentage: -1
     };
 
     return {
@@ -49,6 +63,29 @@ var budgetController = (function() {
 
             // Return the new element.
             return newItem;
+        },
+
+        calculateBudget: function() {
+
+            // Calculate the total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            // Calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // Calculate the percentage of income that we spent
+            data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
         },
 
         testing: function() {
@@ -144,11 +181,15 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
 
     var updateBudget = function() {
+
         // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
 
         // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI
+        console.log(budget);
 
     };
 
